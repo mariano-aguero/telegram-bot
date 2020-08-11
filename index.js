7fcac8c1878037289885dc259b39f7cbb8a58249
Mariano Aguero <mariano.aguero@altoros.com>
2020-08-11 12:49:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api')
 const fs = require('fs')
 
-const token = '798091433:AAHam0ZCha_HSlwDI145hN_BbHtAdU7XSLM'
+const token = 'TELEGRAM_BOT_TOKEN'
 
 const bot = new TelegramBot(token, {polling: true})
 
